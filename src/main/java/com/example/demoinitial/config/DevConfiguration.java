a399b4c07ecfaa0835a8a18cb7e4839b0e8a4ebc
Matthew Machivenyika <accounts@machivenyika.ch>
2023-03-01 10:18:19 +0100

    LE02-6: Task 6

--- src/main/java/com/example/demoinitial/config/DevConfiguration.java
+++ src/main/java/com/example/demoinitial/config/DevConfiguration.java
@@ -1,7 +1,7 @@
 package com.example.demoinitial.config;
 
-import com.example.demoinitial.domain.Person;
-import com.example.demoinitial.repository.PersonRepository;
+import com.example.demoinitial.domain.*;
+import com.example.demoinitial.repository.*;
 import com.example.demoinitial.utils.HasLogger;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,9 +21,6 @@ import java.util.List;
 public class DevConfiguration implements HasLogger {
 
     @Autowired
-    private PersonRepository personRepository;
-
-    @Autowired
     @Qualifier("felixMuster")
     Person felixMuster;
 
@@ -31,11 +28,38 @@ public class DevConfiguration implements HasLogger {
     @Qualifier("maxMustermann")
     Person maxMustermann;
 
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+    @Autowired
+    DepartmentRepository departmentRepository;
+
+    @Autowired
+    AddressRepository addressRepository;
+
+    @Autowired
+    ProjectRepository projectRepository;
+
     public DevConfiguration() {
         getLogger().info("Dev Configuration Class");
     }
 
     @PostConstruct
+    public void createData() {
+        createPersonData();
+        Employee employeeFelixMuster = createEmployeeFelixMuster();
+        Employee employeeJohnDoe = createEmployeeJohnDoe();
+        createDepartment();
+        createPhone(employeeFelixMuster.getId());
+        createDesignProject();
+        createQualityProject();
+        createEmployeeMaxMustermann();
+        assignMaxMustermannToDesignProject();
+        assignMaxMustermannAsChef();
+    }
+
     public void createPersonData() {
         personRepository.save(felixMuster);
         personRepository.save(maxMustermann);
@@ -46,6 +70,130 @@ public class DevConfiguration implements HasLogger {
         personRepository.findAll(Sort.by(Sort.Direction.ASC, "lastName")).forEach(person -> {
             personRepository.findById(person.getId())
                     .ifPresent(p -> getLogger().debug("person.toString() = " + p));
+        });
+    }
+
+    private Employee createEmployeeFelixMuster() {
+        Address address = new Address();
+        address.setCity("Zürich");
+        address.setState("ZH");
+        address.setStreet("Lagerstrasse 41");
+        address.setZip("8004");
+        Employee emp = new Employee();
+        emp.setAddress(address);
+        emp.setName("Felix Muster");
+        emp.setSalary(80000);
+        emp = employeeRepository.save(emp);
+        return emp;
+    }
+
+    private Employee createEmployeeJohnDoe() {
+        Address address = new Address();
+        address.setCity("Basel");
+        address.setState("BS");
+        address.setStreet("Peter-Merianstrasse 41");
+        address.setZip("4001");
+        Phone phoneWork = new Phone();
+        phoneWork.setPhonenumber("061 123 45 67");
+        phoneWork.setType("Work");
+        Employee emp = new Employee();
+        emp.setAddress(address);
+        emp.getPhones().add(phoneWork);
+        emp.setName("John Doe");
+        emp.setSalary(100000);
+        phoneWork.setEmployee(emp);
+        emp = employeeRepository.save(emp);
+        return emp;
+    }
+
+    private Employee createEmployeeMaxMustermann() {
+        Address address = new Address();
+        address.setCity("Aarau");
+        address.setState("AG");
+        address.setStreet("Bahnhofstrasse 41");
+        address.setZip("5001");
+        Phone phoneWork = new Phone();
+        phoneWork.setPhonenumber("062 888 45 67");
+        phoneWork.setType("Work");
+        Employee emp = new Employee();
+        emp.setAddress(address);
+        emp.getPhones().add(phoneWork);
+        emp.setName("Max Mustermann");
+        emp.setSalary(90000);
+        phoneWork.setEmployee(emp);
+        emp = employeeRepository.save(emp);
+        return emp;
+    }
+
+    private void createPhone(Long empId) {
+        Phone phoneWork = new Phone();
+        phoneWork.setPhonenumber("031 999 99 99");
+        phoneWork.setType("Work");
+        Phone phoneHome = new Phone();
+        phoneHome.setPhonenumber("032 333 33 33");
+        phoneHome.setType("Home");
+        employeeRepository.findByIdWithEagerRelationships(empId).ifPresent(emp -> {
+            phoneWork.setEmployee(emp);
+            emp.getPhones().add(phoneWork);
+            phoneHome.setEmployee(emp);
+            emp.getPhones().add(phoneHome);
+            employeeRepository.save(emp);
+        });
+    }
+
+    private void createDepartment() {
+        Department departmentHR = new Department();
+        departmentHR.setName("HR");
+        departmentHR = departmentRepository.save(departmentHR);
+        Department department = new Department();
+        department.setName("Dev");
+        Department departmentDev = departmentRepository.save(department);
+        employeeRepository.findAllWithEagerRelationships().forEach(emp -> {
+            departmentDev.getEmployees().add(emp);
+            emp.setDepartment(departmentDev);
+            employeeRepository.save(emp);
+        });
+        departmentRepository.findByIdWithEagerRelationships(departmentDev.getId()).ifPresent(dep -> {
+            dep.getEmployees().forEach(e -> getLogger().info("Department " + dep.getName() + " - " + e.toString()));
+        });
+    }
+
+    private void createDesignProject() {
+        DesignProject project = new DesignProject("Arcos");
+        project.setInnovationLevel(3);
+        employeeRepository.findAllWithEagerRelationships().forEach(emp -> {
+            project.getEmployees().add(emp);
+            emp.getProjects().add(project);
+        });
+        projectRepository.save(project);
+    }
+
+    private void createQualityProject() {
+        QualityProject project = new QualityProject("My Quality Project");
+        employeeRepository.findAllWithEagerRelationships().forEach(emp -> {
+            project.getEmployees().add(emp);
+            emp.getProjects().add(project);
+        });
+        projectRepository.save(project);
+    }
+
+    private void assignMaxMustermannToDesignProject() {
+        employeeRepository.findByNameWithEagerRelationships("Max Mustermann").ifPresent(employee -> {
+            projectRepository.findWithEagerProjectByName("Arcos").forEach(project -> {
+                project.getEmployees().add(employee);
+                employee.getProjects().add(project);
+                projectRepository.save(project);
+            });
+        });
+    }
+
+    private void assignMaxMustermannAsChef() {
+        employeeRepository.findByNameWithEagerRelationships("Max Mustermann").ifPresent(chef -> {
+            employeeRepository.findByNameWithEagerRelationships("Felix Muster").ifPresent(employee -> {
+                employee.setChef(chef);
+                chef.getEmployees().add(employee);
+                employeeRepository.save(chef);
+            });
         });
     }
 }
---
+++ src/main/java/com/example/demoinitial/domain/Project.java
@@ -0,0 +1,56 @@
+package com.example.demoinitial.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.DiscriminatorType;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.ManyToMany;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorValue(value = "Project")
+@DiscriminatorColumn(name = "type", discriminatorType = DiscriminatorType.STRING)
+public abstract class Project extends BaseEntity {
+    private String name;
+    @Column(name = "type", insertable = false, updatable = false)
+    protected String type;
+    @ManyToMany
+    private Set<Employee> employees = new HashSet<>();
+
+    public Project() {
+    }
+
+    public Project(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(Set<Employee> employees) {
+        this.employees = employees;
+    }
+}
