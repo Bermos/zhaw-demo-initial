dc31647853be6141e361721a385badb9159dbb60
Matthew Machivenyika <accounts@machivenyika.ch>
2023-03-08 09:03:23 +0100

    LE02-7: Task 7

--- src/main/java/com/example/demoinitial/DemoInitialApplication.java
+++ src/main/java/com/example/demoinitial/DemoInitialApplication.java
@@ -13,16 +13,36 @@ import java.util.Arrays;
 @SpringBootApplication
 @Controller
 public class DemoInitialApplication implements HasLogger {
-	@Autowired
-	private Environment env;
+    @Autowired
+    private Environment env;
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoInitialApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoInitialApplication.class, args);
+    }
 
-	@PostConstruct
-	public void afterInit() {
-		boolean hasDevProfile = Arrays.asList(env.getActiveProfiles()).contains("dev");
-		getLogger().info("Active Profiles: " + Arrays.toString(env.getActiveProfiles()) + "\n\n");
-	}
+    @PostConstruct
+    public void afterInit() {
+        boolean hasDevProfile = Arrays.asList(env.getActiveProfiles()).contains("dev");
+        boolean hasH2Database = Arrays.asList(env.getActiveProfiles()).contains("h2");
+        String applicationName = env.getProperty("spring.application.name");
+        String openApiInfo = "";
+        String h2ConsoleInfo = "";
+        if (hasDevProfile) {
+            openApiInfo = """
+                     http://localhost:8080/v3/api-docs
+                     http://localhost:8080/v3/api-docs.yaml -> yaml file is downloaded -> 
+                     https://editor.swagger.io/
+                     http://localhost:8080/swagger-ui.html\s
+                     """;
+        }
+        if (hasH2Database) {
+            h2ConsoleInfo = "http://localhost:8080/h2-console " + "" +
+                    "-> mit Generic H2 (Embedded), org.h2.Driver, jdbc:h2:mem:testdb und sa \n";
+        }
+        System.out.println("\n\nApplication [" + applicationName + "] - Enter in Browser:\nhttp://localhost:8080 \n" +
+                openApiInfo +
+                h2ConsoleInfo + "\n" +
+                "Active Profiles: " + Arrays.toString(env.getActiveProfiles()) + "\n\n");
+        getLogger().info("Active Profiles: " + Arrays.toString(env.getActiveProfiles()) + "\n\n");
+    }
 }
